391c5bd9a20dd07f38f6a37f12078091d2fb3cf9
J0na555 <ethiopediaa@gmail.com>
2025-11-24 21:50:42 +0300

    WIP: exercise + workout base changes

--- backend/apps/workouts/serializers.py
+++ backend/apps/workouts/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Workout
+from .models import Workout, Exercise
 
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model= Workout
         fields = ['id','user', 'title', 'status', 'duration', 'is_public', 'date', 'notes', 'created_at', 'updated_at']
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ['id', 'name', 'description', 'target_muscle', 'muscles_secondary', 'equipment', 'difficulty', 'category', 'is_compound', 'sets', 'reps', 'weight', 'duration', 'distance, rest_time', 'video_url', 'image', 'instructions', 'tips', 'created_at']
+        read_only_fields = ['id', 'target_muscle', 'muscles_secondary', 'instructions', 'tips', 'equipment', 'difficulty', 'created_at']
