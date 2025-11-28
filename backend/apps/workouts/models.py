de4578e35601e94fbda3a0622521305674c13f6c
J0na555 <ethiopediaa@gmail.com>
2025-11-28 16:46:39 +0300

    feat/workout: add workout exercise model

--- backend/apps/workouts/models.py
+++ backend/apps/workouts/models.py
@@ -12,7 +12,7 @@ class Workout(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='workout')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='workouts')
 
     title = models.CharField(max_length=50)
     status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='planned')
@@ -20,10 +20,13 @@ class Workout(models.Model):
 
     duration = models.PositiveIntegerField(null=True, blank=True, help_text='Duration in minutes')
     date = models.DateField()
-    notes = models.TextField(max_length=1000)
+    notes = models.TextField(max_length=1000, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date']
 
     def __str__(self):
         return f"{self.title} - {self.user.username} - {self.date}"
@@ -48,21 +51,42 @@ class Exercise(models.Model):
     category = models.CharField(max_length=50)
     is_compound = models.BooleanField(default=False)
 
-    sets = models.IntegerField(default=0)
-    reps = models.CharField(max_length=50, blank=True)
-    weight = models.FloatField(default=0)
-    
-    duration = models.IntegerField(default=0)  # seconds
-    distance = models.FloatField(default=0)    # meters
-    rest_time = models.IntegerField(default=0) # seconds
-    
     video_url = models.URLField(blank=True)
     image = models.ImageField(upload_to="exercises/", blank=True)
     
     instructions = models.TextField(blank=True)
     tips = models.TextField(blank=True)
 
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.workout.title}"
+        return f"{self.name}"
+
+class WorkoutExercise(models.Model):
+    id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+
+    workout = models.ForeignKey('workouts.Workout', on_delete= models.CASCADE, related_name='workout_exercises')
+    exercise = models.ForeignKey('workouts.Exercise', on_delete= models.CASCADE, related_name='exercise_instances')
+
+
+    sets = models.PositiveIntegerField(default=0)
+    reps = models.CharField(max_length=50, blank=True) 
+    weight = models.FloatField(default=0)  #KG
+
+
+    duration = models.IntegerField(null=True, blank=True)   # seconds
+    distance = models.FloatField(null=True, blank=True)     # meters
+    rest_time = models.IntegerField(null=True, blank=True)  # seconds
+
+
+    user_comment = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        unique_together = ('workout', 'exercise')
+
+    def __str__(self):
+        return f"{self.exercise.name} in {self.workout.title}"
--- backend/apps/workouts/serializers.py
+++ backend/apps/workouts/serializers.py
@@ -1,14 +1,67 @@
 from rest_framework import serializers
-from .models import Workout, Exercise
+from .models import Workout, Exercise, WorkoutExercise
 
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
-        model= Workout
-        fields = ['id','user', 'title', 'status', 'duration', 'is_public', 'date', 'notes', 'created_at', 'updated_at']
+        model = Workout
+        fields = [
+            'id', 'user', 'title', 'status',
+            'duration', 'is_public', 'date', 'notes',
+            'created_at', 'updated_at'
+        ]
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
 
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
-        fields = ['id', 'name', 'description', 'target_muscle', 'muscles_secondary', 'equipment', 'difficulty', 'category', 'is_compound', 'sets', 'reps', 'weight', 'duration', 'distance, rest_time', 'video_url', 'image', 'instructions', 'tips', 'created_at']
-        read_only_fields = ['id', 'target_muscle', 'muscles_secondary', 'instructions', 'tips', 'equipment', 'difficulty', 'created_at']
+        fields = [
+            'id', 'name', 'description',
+            'target_muscle', 'muscles_secondary',
+            'equipment', 'difficulty', 'category',
+            'is_compound', 'video_url', 'image',
+            'instructions', 'tips', 'created_at'
+        ]
+        read_only_fields = ['id', 'created_at']
+
+
+class WorkoutExerciseSerializer(serializers.ModelSerializer):
+    exercise_detail = ExerciseSmallSerializer(source='exercise', read_only=True)
+
+    class Meta:
+        model = WorkoutExercise
+        fields = [
+            'id',
+            'workout',
+            'exercise',
+            'exercise_detail',
+            'sets',
+            'reps',
+            'weight',
+            'duration',
+            'distance',
+            'rest_time',
+            'user_comment',
+            'completed',
+            'created_at'
+        ]
+
+        read_only_fields = ['id', 'created_at', 'exercise_detail']
+
+    def validate(self, data):
+
+        if data.get('sets') is not None and data['sets'] < 0:
+            raise serializers.ValidationError("sets can not be negative")
+
+        if data.get('weight') is not None and data['weight'] < 0:
+            raise serializers.ValidationError("weight  can not be negative")
+        
+        if data.get('duration') is not None and data['duration'] < 0:
+            raise serializers.ValidationError("Duration  can not be negative")
+
+        if data.get('rest_time') is not None and data['rest_time'] < 0:
+            raise serializers.ValidationError("Rest Time  can not be negative")
+
+        if data.get('distance') is not None and data['distance'] < 0:
+            raise serializers.ValidationError("Distance cannot be negative.")
+
+        return data
